99cc8c0827973b63efcabc35652c0652ec4b569e
Khoi Le <khoile@stanford.edu>
2018-02-18 04:20:45 -0800

    yum yum tag creation

--- components/ScanSurroundings.js
+++ components/ScanSurroundings.js
@@ -11,7 +11,7 @@ const COMPVIS_PARAMS = {
 const CUSTVIS_ID = "c1e36469-83dc-433c-8044-fa7ef7f31117";
 const CUSTVIS_KEY = "8b0f96ec859f40d68efc87fde36f8a11";
 const COMPVIS_URL = `https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=${COMPVIS_PARAMS.visualFeatures}&language=${COMPVIS_PARAMS.language}`;
-const CUSTVIS_URL = `https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/c1e36469-83dc-433c-8044-fa7ef7f31117/images?tagIds%5B%5D=e8a935f3-e61f-4f31-b8d2-25388809c2f2`// + //encodeURIComponent(JSON.stringify(["e8a935f3-e61f-4f31-b8d2-25388809c2f2"]));
+const CUSTVIS_URL = `https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/c1e36469-83dc-433c-8044-fa7ef7f31117/images?tagIds%5B%5D=`// + //encodeURIComponent(JSON.stringify(["e8a935f3-e61f-4f31-b8d2-25388809c2f2"]));
 export default class ScanSurroundings extends React.Component {
   static navigationOptions = {
     header: null,
@@ -54,14 +54,12 @@ export default class ScanSurroundings extends React.Component {
       ratio,
     });
   }
+
   sendPhotoCompVis(fileLoc) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.responseType="json";
     xmlHttp.onreadystatechange = (e) => {
-      //console.log("changed: " + xmlHttp.readyState);
       if(xmlHttp.readyState == 4){
-        //console.log("ready: " + xmlHttp.status);
-
         if(xmlHttp.status === 200){
           console.log(xmlHttp.response.description.captions[0].text);
           console.log(xmlHttp.response.description.tags[3]);
@@ -88,9 +86,6 @@ export default class ScanSurroundings extends React.Component {
     xmlHttp.open( "POST", COMPVIS_URL, true);
     xmlHttp.setRequestHeader("Content-Type","multipart/form-data");
     xmlHttp.setRequestHeader("Ocp-Apim-Subscription-Key",COMPVIS_KEY);
-    // var jsonBody = {
-    //   "url": `./photos/Photo_${this.state.photoId - 1}.jpg`
-    // }
     const data = new FormData();
     data.append('photo', {
       uri: fileLoc,
@@ -100,64 +95,63 @@ export default class ScanSurroundings extends React.Component {
     xmlHttp.send(data);
   }
 
-  sendPhotoTags(tagData) {
-    console.log(tagData);
-    var POSTTAG_URL = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/c1e36469-83dc-433c-8044-fa7ef7f31117/images/tags";
-    //this.getTags();
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.responseType="json";
-    xmlHttp.onreadystatechange = (e) => {
-      console.log(xmlHttp.readyState);
-      if(xmlHttp.readyState == 4){
-        if(xmlHttp.status === 200){
-          console.log(xmlHttp.response);
-        }
-        else {
-          console.log(xmlHttp.response);
-        }
-      }
-    }
-    xmlHttp.open( "POST", POSTTAG_URL, true);
-    xmlHttp.setRequestHeader("Content-Type","application/json");
-    xmlHttp.setRequestHeader("Training-key",CUSTVIS_KEY);
-    xmlHttp.send(tagData);
-  }
-  tagUntaggedPhotos(tagName){
-    var UNTAGGED_URL = `https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/${CUSTVIS_ID}/images/untagged`;
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.responseType="json";
-    xmlHttp.onreadystatechange = (e) => {
-      console.log(xmlHttp.readyState);
-      if(xmlHttp.readyState == 4){
-        if(xmlHttp.status === 200){
-          console.log(xmlHttp.response);
-          var photoIds = [];
-          for(var i = 0; i < xmlHttp.response.length; i++){
-            console.log(xmlHttp.response[i].Id);
-            photoIds.push(xmlHttp.response[i].Id);
-          }
-
-          //for(var i = 0; i < photoIds.length; i++){
-            var taggedPhotos = {
-              "Tags": [
-                {
-                  "ImageId": photoIds[0],
-                  "TagId": tagName
-                }
-              ]
-            }
-            this.sendPhotoTags(taggedPhotos);
-          }
-        //}
-        else {
-          console.log(xmlHttp.response);
-        }
-      }
-    }
-    xmlHttp.open( "GET", UNTAGGED_URL, true);
-    xmlHttp.setRequestHeader("Training-key",CUSTVIS_KEY);
-    xmlHttp.send();
-  }
+  // sendPhotoTags(tagData) {
+  //   console.log(tagData);
+  //   var POSTTAG_URL = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/c1e36469-83dc-433c-8044-fa7ef7f31117/images/tags";
+  //   var xmlHttp = new XMLHttpRequest();
+  //   xmlHttp.responseType="json";
+  //   xmlHttp.onreadystatechange = (e) => {
+  //     console.log(xmlHttp.readyState);
+  //     if(xmlHttp.readyState == 4){
+  //       if(xmlHttp.status === 200){
+  //         console.log(xmlHttp.response);
+  //       }
+  //       else {
+  //         console.log(xmlHttp.response);
+  //       }
+  //     }
+  //   }
+  //   xmlHttp.open( "POST", POSTTAG_URL, true);
+  //   xmlHttp.setRequestHeader("Content-Type","application/json");
+  //   xmlHttp.setRequestHeader("Training-key",CUSTVIS_KEY);
+  //   xmlHttp.send(tagData);
+  // }
+  // tagUntaggedPhotos(tagName){
+  //   var UNTAGGED_URL = `https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/${CUSTVIS_ID}/images/untagged`;
+  //   var xmlHttp = new XMLHttpRequest();
+  //   xmlHttp.responseType="json";
+  //   xmlHttp.onreadystatechange = (e) => {
+  //     console.log(xmlHttp.readyState);
+  //     if(xmlHttp.readyState == 4){
+  //       if(xmlHttp.status === 200){
+  //         console.log(xmlHttp.response);
+  //         var photoIds = [];
+  //         for(var i = 0; i < xmlHttp.response.length; i++){
+  //           console.log(xmlHttp.response[i].Id);
+  //           photoIds.push(xmlHttp.response[i].Id);
+  //         }
+  //
+  //         //for(var i = 0; i < photoIds.length; i++){
+  //           var taggedPhotos = {
+  //             "Tags": [
+  //               {
+  //                 "ImageId": photoIds[0],
+  //                 "TagId": tagName
+  //               }
+  //             ]
+  //           }
+  //           this.sendPhotoTags(taggedPhotos);
+  //         }
+  //       //}
+  //       else {
+  //         console.log(xmlHttp.response);
+  //       }
+  //     }
+  //   }
+  //   xmlHttp.open( "GET", UNTAGGED_URL, true);
+  //   xmlHttp.setRequestHeader("Training-key",CUSTVIS_KEY);
+  //   xmlHttp.send();
+  // }
   getTags(){
     var TAGS_URL = `https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/${CUSTVIS_ID}/tags`;
     var xmlHttp = new XMLHttpRequest();
@@ -182,7 +176,7 @@ export default class ScanSurroundings extends React.Component {
     xmlHttp.setRequestHeader("Training-key",CUSTVIS_KEY);
     xmlHttp.send();
   }
-  sendPhotoCustVis(formData){
+  sendPhotoCustVis(formData, tagId){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.responseType="json";
     xmlHttp.onreadystatechange = (e) => {
@@ -190,36 +184,53 @@ export default class ScanSurroundings extends React.Component {
       if(xmlHttp.readyState == 4){
         if(xmlHttp.status === 200){
           console.log(xmlHttp.response);
-          this.tagUntaggedPhotos("e8a935f3-e61f-4f31-b8d2-25388809c2f2");
+          //this.tagUntaggedPhotos("e8a935f3-e61f-4f31-b8d2-25388809c2f2");
         }
         else {
           console.log(xmlHttp.responseJson);
         }
       }
     }
-    xmlHttp.open( "POST", CUSTVIS_URL, true);
+    var url = CUSTVIS_URL + tagId;
+    xmlHttp.open( "POST", url, true);
     xmlHttp.setRequestHeader("Content-Type","multipart/form-data");
     xmlHttp.setRequestHeader("Training-key",CUSTVIS_KEY);
     xmlHttp.send(formData);
   }
-  createTaggedPhoto(){
-    // UI prompt for user to move phone around object
-    // take periodic photos, add to form data
-    // UI prompt for tag
-    // UI Prompt for description/info
-    // send form data
+  createTag(photoData, tag, description) {
+    var CREATE_TAG_URL = `https://southcentralus.api.cognitive.microsoft.com/customvision/v1.2/Training/projects/${CUSTVIS_ID}/tags?name=${tag}&description=${description}`;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.responseType="json";
+    xmlHttp.onreadystatechange = (e) => {
+      console.log(xmlHttp.readyState);
+      if(xmlHttp.readyState == 4){
+        if(xmlHttp.status === 200){
+          console.log(xmlHttp.response);
+          this.sendPhotoCustVis(photoData, xmlHttp.response.Id);
+        }
+        else {
+          console.log(xmlHttp.responseJson);
+        }
+      }
+    }
+    xmlHttp.open( "POST", CREATE_TAG_URL, true);
+    xmlHttp.setRequestHeader("Training-key",CUSTVIS_KEY);
+    xmlHttp.send();
+  }
+
+  photoBurst(ctx) {
     const photoData = new FormData();
-    var fileLoc = `${FileSystem.documentDirectory}photos/Photo_${this.state.photoId}.jpg`;
-    if(this.camera) {
-      this.camera.takePictureAsync().then(data => {
+    var fileLoc = `${FileSystem.documentDirectory}photos/Photo_${ctx.state.photoId}.jpg`;
+    if(ctx.camera) {
+      ctx.camera.takePictureAsync().then(data => {
         FileSystem.moveAsync({
           from: data.uri,
           to: fileLoc,
         }).then(() => {
-          this.setState({
-            photoId: this.state.photoId + 1,
+          ctx.setState({
+            photoId: ctx.state.photoId + 1,
           });
-          Vibration.vibrate();
+
           photoData.append('photo', {
             uri: fileLoc,
             type: 'image/jpeg',
@@ -231,10 +242,56 @@ export default class ScanSurroundings extends React.Component {
               "puppy"
             ]
           });
-          this.sendPhotoCustVis(photoData);
+          var tag, description;
+          tag = "test234234";
+          description = "test description, it is super good, SOOOOO great, amazing, wauw, so good, wow, excellent description!";
+          ctx.createTag(photoData, tag, description);
         });
       });
     }
+  }
+
+  createTaggedPhoto(){
+    // UI prompt for user to move phone around object
+    // take periodic photos, add to form data
+    // UI prompt for tag
+    // UI Prompt for description/info
+    // send form data
+    var counter = 0;
+    var photoBurst = setInterval(function(this){
+      counter++;
+      
+      const photoData = new FormData();
+      var fileLoc = `${FileSystem.documentDirectory}photos/Photo_${ctx.state.photoId}.jpg`;
+      if(ctx.camera) {
+        ctx.camera.takePictureAsync().then(data => {
+          FileSystem.moveAsync({
+            from: data.uri,
+            to: fileLoc,
+          }).then(() => {
+            ctx.setState({
+              photoId: ctx.state.photoId + 1,
+            });
+
+            photoData.append('photo', {
+              uri: fileLoc,
+              type: 'image/jpeg',
+              name: 'photo',
+              TagsIds: [
+                "puppy"
+              ],
+              Tags: [
+                "puppy"
+              ]
+            });
+            var tag, description;
+            tag = "test234234";
+            description = "test description, it is super good, SOOOOO great, amazing, wauw, so good, wow, excellent description!";
+            ctx.createTag(photoData, tag, description);
+          });
+        });
+      }
+    }, 100);
 
   }
   takePicture = async function() {
