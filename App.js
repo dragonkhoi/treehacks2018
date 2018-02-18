b50ada8d922c042557073344890468d0908aa13e
Khoi Le <khoile@stanford.edu>
2018-02-17 17:25:33 -0800

    yayyyy got a camera to work...

--- App.js
+++ App.js
@@ -1,21 +1,102 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, Vibration } from 'react-native';
+import { Camera, Permissions, FileSystem, Constants } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    flash: 'off',
+    zoom: 0,
+    autoFocus: 'on',
+    depth: 0,
+    ratio: '16:9',
+    ratios: [],
+    photoId: 1,
+    whiteBalance: 'auto',
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  componentDidMount() {
+    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'photos').catch(e => {
+      console.log(e, 'Directory exists');
+    });
+  }
+
+  getRatios = async () => {
+    const ratios = await this.camera.getSupportedRatios();
+    return ratios;
+  }
+
+  setRatio(ratio) {
+    this.setState({
+      ratio,
+    });
+  }
+
+  takePicture = async function() {
+    if(this.camera) {
+      this.camera.takePictureAsync().then(data => {
+        FileSystem.moveAsync({
+          from: data.uri,
+          to: `${FileSystem.documentDirectory}photos/Photo_${this.state.photoId}.jpg`,
+        }).then(() => {
+          this.setState({
+            photoId: this.state.photoId + 1,
+          });
+          Vibration.vibrate();
+        });
+      });
+    }
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={{ flex: 1 }}
+            type={this.state.type}
+            flashMode={this.state.flash}
+            autoFocus={this.state.autoFocus}
+            zoom={this.state.zoom}
+            whiteBalance={this.state.whiteBalance}
+            ratio={this.state.ratio}
+            focusDepth={this.state.depth}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={this.takePicture.bind(this)}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}TRANSLATE{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
